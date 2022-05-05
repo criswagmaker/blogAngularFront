e014584384cd4ea014805469cd77499d0f849e9f
Criswagmaker <cristian.wagmakerpessoal@gmail.com>
2022-05-05 00:56:10 -0300

    Consumo de API - Login e Cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 const routes: Routes = [
 {path: '', redirectTo: 'entrar', pathMatch: 'full'},
 
 {path:'entrar', component: EntrarComponent},
-{path:'cadastrar', component: CadastrarComponent}
+{path:'cadastrar', component: CadastrarComponent},
 
+{path: 'inicio', component: InicioComponent}
 
 ];
 
@@ -16,4 +18,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { UsuarioLogin } from '../model/UsuarioLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-entrar',
   templateUrl: './entrar.component.html',
   styleUrls: ['./entrar.component.css']
 })
+
 export class EntrarComponent implements OnInit {
 
-  constructor() { }
+  usuarioLogin: UsuarioLogin = new UsuarioLogin()
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    window.scroll(0, 0)
+  }
+
+  entrar() {
+    this.auth.entrar(this.usuarioLogin).subscribe((resp: UsuarioLogin) => {
+      this.usuarioLogin = resp
+
+      environment.token = this.usuarioLogin.token
+      environment.nome = this.usuarioLogin.nome
+      environment.foto = this.usuarioLogin.foto
+      environment.id = this.usuarioLogin.id
+
+      this.router.navigate(['/inicio'])
+    }, erro => {
+      if (erro.status == 500) {
+        alert('Usuário ou senha estão Incorretos!')
+      }
+    })
   }
 
 }
