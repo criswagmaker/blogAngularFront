81d0d73f05775c3207a21d655c7ed9bcfc6bbb0c
Criswagmaker <cristian.wagmakerpessoal@gmail.com>
2022-05-09 14:24:02 -0300

    Implementação do get e post de temas

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -1,17 +1,24 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { Tema } from '../model/Tema';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TemaService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-}
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token),
+  };
 
-/*   postTema(tema: Tema): Observable<Tema>{
-    return this.http.post<Tema>('https://blogpessoal')
+  getAllTema(): Observable<Tema[]> {
+    return this.http.get<Tema[]>('http://localhost:8080/tema', this.token);
   }
-} */
+
+  postTema(tema: Tema): Observable<Tema> {
+    return this.http.post<Tema>('http://localhost:8080/tema', tema, this.token);
+  }
+}
--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,23 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { find } from 'rxjs';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 import { environment } from './../../environments/environment.prod';
 
 @Component({
   selector: 'app-tema',
   templateUrl: './tema.component.html',
-  styleUrls: ['./tema.component.css']
+  styleUrls: ['./tema.component.css'],
 })
 export class TemaComponent implements OnInit {
+  tema: Tema = new Tema();
+  listaTemas: Tema[];
 
-  constructor(
-    private router: Router
-  ) { }
+  constructor(private router: Router, private temaService: TemaService) {}
 
   ngOnInit() {
     if (environment.token == '') {
       /* alert('Sessão expirada, faça o login novamente'); */
       this.router.navigate(['/entrar']);
     }
+
+    this.findAllTemas();
   }
 
+  findAllTemas() {
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp;
+    });
+  }
+
+  cadastrar() {
+    this.temaService.postTema(this.tema).subscribe((resp: Tema) => {
+      this.tema = resp;
+      alert('Tema cadastrado com sucesso!');
+      this.findAllTemas();
+      this.tema = new Tema();
+    });
+  }
 }
