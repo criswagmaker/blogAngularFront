9bba7477a06bec28f29fb5fadd1ea97c70535e64
Criswagmaker <cristian.wagmakerpessoal@gmail.com>
2022-05-12 21:25:23 -0300

    Implementação do post e get de postagens

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -6,30 +6,50 @@ import { Usuario } from '../model/Usuario';
 import { UsuarioLogin } from '../model/UsuarioLogin';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token),
+  };
+
+  refreshToken() {
+    this.token = {
+      headers: new HttpHeaders().set('Authorization', environment.token),
+    };
+  }
 
   entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin> {
-    return this.http.post<UsuarioLogin>('http://localhost:8080/usuarios/logar', usuarioLogin)
+    return this.http.post<UsuarioLogin>(
+      'http://localhost:8080/usuarios/logar',
+      usuarioLogin
+    );
   }
 
   cadastrar(usuario: Usuario): Observable<Usuario> {
-    return this.http.post<Usuario>('http://localhost:8080/usuarios/cadastrar', usuario)
+    return this.http.post<Usuario>(
+      'http://localhost:8080/usuarios/cadastrar',
+      usuario
+    );
   }
 
-  logado(){
-    let ok: boolean = false
+  getByIdUser(id: number): Observable<Usuario> {
+    return this.http.get<Usuario>(
+      `http://localhost:8080/usuarios/${id}`,
+      this.token
+    );
+  }
 
-    if (environment.token !=  ''){
-      ok = true
+  logado() {
+    let ok: boolean = false;
+
+    if (environment.token != '') {
+      ok = true;
     }
 
-    return ok
+    return ok;
   }
 }
